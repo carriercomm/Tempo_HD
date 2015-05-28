12f8bfce84384a71b77038d0ed159c2e25c3300b
unknown <JXP1195@C9090E6D717E228.amer.homedepot.com>
2015-05-28 14:18:19 -0400

    Fixed issue with topojson file path, removed Tracker.autorun outer wrapper from map rendering logic

--- beacon_hd.js
+++ beacon_hd.js
@@ -6,22 +6,29 @@ if(Meteor.isServer){
 if(Meteor.isClient){
 	
   Template.map.rendered = function(){
-    var svg, width = 1200, height = 800;
+    var svg, width = 1600, height = 1200;
 
     svg = d3.select('#map').append('svg')
         .attr('width', width)
         .attr('height', height);
+				
+		var path = d3.geo.path();
 		
-		Tracker.autorun(function(){
-			d3.json('public/thd_states.json', function(states){
-				//var path = d3.geo.path();
+		// Commented out this wrapper, I think this file should only be loaded once, on render. This will be 
+		// moved to an inner location once we begin loading the store data.
+		//Tracker.autorun(function(){
+			
+			d3.json('/thd_states.json', function(states){
+				
 				
 				console.log(states);
 				
-				//svg.selectAll('path').data(states)
+				svg.selectAll('path').data(topojson.feature(states, states.objects.na_states_places).features)
+					.enter().append('path')
+						.attr('d',path);
 			});
 			
-		});
+		//});
 		
   };
 }
