d174c2912b1d3ffc1fd250014de42493517738d9
unknown <JXP1195@C9090E6D717E228.amer.homedepot.com>
2015-06-10 12:23:07 -0400

    Added rudimentary hover and click boxes

--- beacon_hd/client/main.js
+++ beacon_hd/client/main.js
@@ -24,70 +24,104 @@ Template.map.onRendered(function(){
 		
 	var path = d3.geo.path().projection(projection);
 
+    var hoverbox = d3.select('body').append('div')
+        .attr('class', 'hoverbox')
+        .style('opacity', 0);
+
+    var clickbox = d3.select('body').append('div')
+        .attr('class', 'clickbox')
+        .style('opacity', 0)
+        .on('click', function(){
+            d3.select(this).transition()
+                .duration(500)
+                .style("opacity", 0)
+                .style('pointer-events', 'none');
+        });
+
 	d3.json('/hdgeo.json', function(states){
 
 		console.log(states);
 		
-		var stores = topojson.feature(states, states.objects.store_geo_features).features;
+		//var stores = topojson.feature(states, states.objects.store_geo_features).features;
 		var political_borders_01 = topojson.feature(states, states.objects.world).features;
-		
-		
 
-			
-				svg.selectAll('path')
-				.data(political_borders_01)
-				.enter().append('path')
-					.attr('class', function(d){
-						var id = d.properties.SU_A3;
-						return 'cid_' + id;	
-					})
-					.attr('d',path);
+		svg.selectAll('path')
+		.data(political_borders_01)
+		.enter().append('path')
+			.attr('class', function(d){
+				var id = d.properties.SU_A3;
+				return 'cid_' + id;
+			})
+			.attr('d',path);
 
-				Tracker.autorun(function(){
-					if(stores_handle.ready()){
-					// Checks to make sure data from mongo has loaded:
+		Tracker.autorun(function(){
+			if(stores_handle.ready()){
+			// Checks to make sure data from mongo has loaded:
 
-						var store_status = StoreStatus.find().fetch();
+				var store_status = StoreStatus.find().fetch();
+                console.log(store_status);
+				var circles = svg.selectAll("circle")
+					.data(store_status)
+						.style('fill', function(d){
 
-						var circles = svg.selectAll("circle")
-							.data(store_status)
-								.style('fill', function(d){
+					//var status = StoreStatus.find({num: d.properties.num}).fetch()[0];
+					return d.status;
+					/*if (status) {
+					 //console.log(status);
+					 return status.status;
+					 } else {
+					 return 'GREEN';
+					 }*/
+				})
+					.attr('stroke', 'darkgray')
+					.enter().append('circle')
+						.attr('transform', function(d) {
+							//var coords = d.geometry.coordinates;
+							//console.log(d);
+							//console.log(Number(d.lng) + ' ' + Number(d.lat));
+							return 'translate(' + projection([Number(d.lng), Number(d.lat)]) + ')';
+						})
+						.attr('class', 'place')
+						.attr('r', 4)
+						//.style('fill', '#76FF03')
+						// Sample meteor code to bind the pin color to values in the mongodb
+						.style('fill', function(d){
 
 							//var status = StoreStatus.find({num: d.properties.num}).fetch()[0];
 							return d.status;
 							/*if (status) {
-							 //console.log(status);
-							 return status.status;
-							 } else {
-							 return 'GREEN';
-							 }*/
+								//console.log(status);
+								return status.status;
+							} else {
+								return 'GREEN';
+							}*/
 						})
-							.attr('stroke', 'darkgray')
-							.enter().append('circle')
-								.attr('transform', function(d) {
-									//var coords = d.geometry.coordinates;
-									//console.log(d);
-									//console.log(Number(d.lng) + ' ' + Number(d.lat));
-									return 'translate(' + projection([Number(d.lng), Number(d.lat)]) + ')';
-								})
-								.attr('class', 'place')
-								.attr('r', 4)
-								//.style('fill', '#76FF03')
-								// Sample meteor code to bind the pin color to values in the mongodb
-								.style('fill', function(d){
-
-									//var status = StoreStatus.find({num: d.properties.num}).fetch()[0];
-									return d.status;
-									/*if (status) {
-										//console.log(status);
-										return status.status;
-									} else {
-										return 'GREEN';
-									}*/
-								})
-								.attr('stroke', 'darkgray');
-					}
-				});
+						.attr('stroke', 'darkgray')
+                        .on("mouseover", function(d) {
+                            hoverbox.transition()
+                                .duration(200)
+                                .style("opacity", .8);
+                            hoverbox .html(d.num + "<br/>"  + d.status)
+                                .style("left", (d3.event.pageX) + "px")
+                                .style("top", (d3.event.pageY - 28) + "px");
+                        })
+                        .on("mouseout", function(d) {
+                            hoverbox.transition()
+                                .duration(500)
+                                .style("opacity", 0);
+                        })
+                    .on('click', function(d){
+                        hoverbox.style('opacity',0);
+                        clickbox.transition()
+                            .duration(200)
+                            .style("opacity", .8);
+                        clickbox .html(d.num + "<br/>"  + d.status)
+                            .style("left", (d3.event.pageX) + "px")
+                            .style("top", (d3.event.pageY - 28) + "px")
+                            .style('pointer-events', 'auto');
+                    });
+			}
+		});
 
 		
 	});
