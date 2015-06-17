b5181ca642e4e3dc7f299e0f9eab5f4ebb6b21a0
unknown <JXP1195@C9090E6D717E228.amer.homedepot.com>
2015-06-17 11:30:37 -0400

    Map redrawn with statelines and the Great Lakes, classes added to country and state polygons

--- beacon_hd/client/main.js
+++ beacon_hd/client/main.js
@@ -7,19 +7,11 @@ Template.map.onRendered(function(){
 	svg = d3.select('#map').append('svg')
 			.attr('width', width)
 			.attr('height', height);
-
-	// Different projection, looks a little funky on the edges,
-	// might be good for a USA only map.
-	/*var projection = d3.geo.conicConformal()
-    .rotate([98, 0])
-    .center([0, 38])
-    .parallels([29.5, 45.5])
-    .scale(500)
-    .translate([width / 2, height / 2]);*/
 		
 	var projection = d3.geo.mercator()
-		.scale(723)
-		.center([-100,44])
+		.scale(700)
+        .center([-100,34])
+        //.center([-105,49])
 		.translate([width/2, height/2]);
 		
 	var path = d3.geo.path().projection(projection);
@@ -38,63 +30,56 @@ Template.map.onRendered(function(){
                 .style('pointer-events', 'none');
         });
 
-	d3.json('/hdgeo.json', function(states){
+	d3.json('/world_hi_lakes_topo.json', function(world){
 
-		console.log(states);
-		
-		//var stores = topojson.feature(states, states.objects.store_geo_features).features;
-		var political_borders_01 = topojson.feature(states, states.objects.world).features;
+		console.log(world);
 
-		svg.selectAll('path')
-		.data(political_borders_01)
+        // Transforms a featureset from the topo file into an array of objects (features).
+        var countries = topojson.feature(world, world.objects.countries).features;
+        var us_states = topojson.feature(world, world.objects.states).features;
+
+		svg.selectAll('.countries')
+		.data(countries)
 		.enter().append('path')
-			.attr('class', function(d){
-				var id = d.properties.SU_A3;
-				return 'cid_' + id;
-			})
+            .attr('stroke','white')
+            .attr('class', function(d) {
+                var id = d.properties.SU_A3;
+                return 'countries ' + id;
+            })
 			.attr('d',path);
 
+        svg.selectAll('.states')
+            .data(us_states)
+            .enter().append('path')
+                .attr('stroke','white')
+                .attr('class', function(d){
+                    var id = d.properties.iso_3166_2.substr(0,2);
+                    return 'states ' + id;
+                })
+                .attr('d',path);
+
 		Tracker.autorun(function(){
+
+		    // Checks to make sure data from mongo has loaded:
 			if(stores_handle.ready()){
-			// Checks to make sure data from mongo has loaded:
+
 
 				var store_status = StoreStatus.find().fetch();
 
 				var circles = svg.selectAll("circle")
 					.data(store_status)
 						.style('fill', function(d){
-
-					//var status = StoreStatus.find({num: d.properties.num}).fetch()[0];
-					return d.status;
-					/*if (status) {
-					 //console.log(status);
-					 return status.status;
-					 } else {
-					 return 'GREEN';
-					 }*/
-				})
-					.attr('stroke', 'darkgray')
+					        return d.status;
+				        })
+					    .attr('stroke', 'darkgray')
 					.enter().append('circle')
 						.attr('transform', function(d) {
-							//var coords = d.geometry.coordinates;
-							//console.log(d);
-							//console.log(Number(d.lng) + ' ' + Number(d.lat));
 							return 'translate(' + projection([Number(d.lng), Number(d.lat)]) + ')';
 						})
 						.attr('class', 'place')
 						.attr('r', 4)
-						//.style('fill', '#76FF03')
-						// Sample meteor code to bind the pin color to values in the mongodb
 						.style('fill', function(d){
-
-							//var status = StoreStatus.find({num: d.properties.num}).fetch()[0];
 							return d.status;
-							/*if (status) {
-								//console.log(status);
-								return status.status;
-							} else {
-								return 'GREEN';
-							}*/
 						})
 						.attr('stroke', 'darkgray')
                         .on("mouseover", function(d) {
