445cc12a4c75d102daa97541423e61b676607442
unknown <KXK8563@C9090970EE68B92.amer.homedepot.com>
2015-06-24 15:48:14 -0400

    Changed testData.json to include all stores

--- beacon_hd/server/main.js
+++ beacon_hd/server/main.js
@@ -10,6 +10,7 @@ Meteor.startup(function(){
 		var len = test_data.length, i = 0;
 		for(i;i<len;i++){
 			//console.log(test_data[i]);
+			test_data[i].metrics = {"info1": "-54.333","info2": "-55.333","info3": "-56.333","info4": "-57.333","info5": "-54.333","info6": "-55.333","info7": "-56.333","info8": "-57.333","info9": "-54.333","info10": "-55.333","info11": "-56.333","info12": "-57.333","info13": "-54.333","info14": "-55.333","info15": "-56.333","info16": "-57.333"};
 			StoreStatus.insert(test_data[i]);
 		}
 	}
