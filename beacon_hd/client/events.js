29b8adac8becd81c7ec5f2dbad1f9dc0e75bc232
unknown <JXP1195@C9090E6D717E228.amer.homedepot.com>
2015-06-25 15:23:18 -0400

    Modularized "Clickbox"

--- beacon_hd/client/events.js
+++ beacon_hd/client/events.js
@@ -7,20 +7,6 @@ Template.test.events = {
     }
 }
 
-//Event for close button on clickbox
-Template.clickbox.events = {
-    'click span#close': function () {
-        //Fade clickbox out
-        d3.select('#clickbox').transition()
-            .duration(400)
-            .style('opacity',0);
-        //reset html so that it doesn't repeat itself
-        d3.select('#clickbox').html('<span id="close" style="cursor:pointer">X</span>').style('pointer-events','none');
-        //take out dim from map
-        d3.select('svg').classed('dimming',false);
-    }
-}
-
 //Template.map.events = {
 //    'click span#close': function () {
 //        d3.select('#map').attr('class','');
