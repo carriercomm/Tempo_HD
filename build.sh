310a268dc139fb535a6fb8343ffd3df62db0d5ad
David Boardman <david_boardman1@homedepot.com>
2015-07-06 10:42:31 -0400

    Make changes to build a Docker image from the sourcecode repository.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# If a command-line argument was passed in
+if [ $1 ]
+then
+    echo "Build TAG: $1"
+    TAG=$1
+else
+    # manually set the build tag
+    echo "Use hardcoded build TAG"
+    TAG=1.0
+fi
+
+DOCKER_REPO=docker-registry.ems.homedepot.com
+NAME=tempo-meteor-d3
+IMAGE=${DOCKER_REPO}/${NAME}:${TAG}
+
+echo "--> Image name: ${IMAGE}"
+
+docker build -t ${IMAGE} .
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,26 +13,25 @@ apt-get upgrade
 apt-get -y install curl imagemagick git python-software-properties
 
 ##############################
-# Configure package repositories
-##############################
-
-# Node.js
-add-apt-repository ppa:chris-lea/node.js
-
-apt-get update
-
-##############################
 # Install packages
 ##############################
 
-# Node.js
-apt-get -y install nodejs
+NODE_VERSION=0.12.5
+curl -SLO "http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz" \
+  && tar -xzf "node-v$NODE_VERSION-linux-x64.tar.gz" -C /usr/local --strip-components=1 \
+  && rm "node-v$NODE_VERSION-linux-x64.tar.gz"
 
-# Install the rest with the vagrant user
-su vagrant
+# Check if 'vagrant' user exists, if so execute this
+getent passwd vagrant > /dev/null 2>&1 && ret=true
 
-# Fix resolve problem with git:// URLs
-git config --global url."https://".insteadOf git://
+if $ret
+then
+    # Install the rest with the vagrant user
+    su vagrant
+
+    # Fix resolve problem with git:// URLs
+    git config --global url."https://".insteadOf git://
+fi
 
 # Meteor
 curl https://install.meteor.com | /bin/sh
